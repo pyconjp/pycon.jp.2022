b40a4c80a4a16ac4b9bffaf0a66fa9ac140ba45e
Akira Inamori <ainamori@iij.ad.jp>
2022-07-09 18:05:23 +0900

    基調講演の文字ぐらいまで

--- components/Domains/TopPage/ConferenceSection.vue
+++ components/Domains/TopPage/ConferenceSection.vue
@@ -1,57 +1,42 @@
 <template>
-  <div class="my-8 text-black font-noto">
-    <div>
-      <div class="lg:ml-28 -mb-36">
-        <p
-          class="-mb-8 text-4xl font-bold font-mont lg:text-6xl text-blue-green"
-        >
-          CONFERENCE
-        </p>
-      </div>
-      <div class="w-5/6 mt-10 conference lg:w-7/12">
-        <div class="w-full mb-16 ml-8 lg:ml-80">
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <p>カンファレンス</p>
-        </div>
-        <div class="content-center w-full m-2 text-center lg:ml-80">
-          <div class="text-2xl font-bold font-mont">Keynote</div>
+  <div>
+    <div class="relative">
+      <div class="flex conference">
+        <br />
+        <div class="title text-primary-700">CONFERENCE</div>
+        <div class="text-xl text-bold text-tartiary-900">カンファレンス</div>
+
+        <!-- <div class="content-center text-center"> -->
+        <div class="frame176">
+          <div class="text-3xl text-bold text-tartiary-900">Keynote</div>
           <div class="text-base text-gray-600">基調講演</div>
-          <div class="w-full mt-12 ml-4 text-left">
-            <div
-              v-for="keynote in $i18n.locale === 'ja' ? keynoteJp : keynoteEn"
-              :key="keynote.name"
-              class="flex flex-col mt-8 lg:flex-row"
-            >
-              <div class="flex justify-center w-full mb-6 lg:w-1/4 lg:mb-0">
-                <div class="flex lg:h-full lg:justify-end lg:items-start">
-                  <img
-                    :src="require(`@/assets/images/${keynote.image}`)"
-                    class="self-center"
-                  />
-                </div>
+          <div
+            v-for="keynote in $i18n.locale === 'ja' ? keynoteJp : keynoteEn"
+            :key="keynote.name"
+            class="flex flex-col mt-8 lg:flex-row"
+          >
+            <div class="flex justify-center w-full mb-6 lg:w-1/4 lg:mb-0">
+              <div class="flex lg:h-full lg:justify-end lg:items-start">
+                <img
+                  :src="require(`@/assets/images/${keynote.image}`)"
+                  class="self-center"
+                />
               </div>
-              <div class="flex-1 mb-10 ml-6">
-                <div class="text-xs text-gray-600">{{ keynote.date }}</div>
-                <div
-                  v-if="keynote.title"
-                  class="
-                    my-4
-                    text-lg
-                    font-bold
-                    lg:text-xl
-                    text-blue-green
-                    corner
-                  "
-                >
-                  {{ keynote.title }}
-                </div>
-                <div class="my-2 text-base font-bold">{{ keynote.name }}</div>
-                <p class="text-sm leading-7" v-html="keynote.desc"></p>
+            </div>
+            <div class="flex-1 mb-10 ml-6">
+              <div class="text-xs text-gray-600">
+                {{ keynote.date }}
               </div>
+              <div
+                v-if="keynote.title"
+                class="my-4 text-lg font-bold lg:text-xl text-blue-green corner"
+              >
+                {{ keynote.title }}
+              </div>
+              <div class="my-2 text-base font-bold">
+                {{ keynote.name }}
+              </div>
+              <p class="text-sm leading-7" v-html="keynote.desc"></p>
             </div>
           </div>
         </div>
@@ -111,11 +96,6 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 60px 16px 36px;
-  gap: 36px;
-
-  width: 394px;
-  height: 2396.91px;
 
   /* secondary/secondary-400 */
   background: #f3d34f;
@@ -130,34 +110,36 @@ export default {
   flex-grow: 0;
 }
 
-.grey-background {
-  background: rgba(221, 221, 221, 0.2);
+.title {
+  /* h1 */
+  font-family: 'Bayon';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 64px;
+  line-height: 100%;
+
+  /* or 64px */
+  text-align: center;
+  letter-spacing: 0.02em;
+
+  /* shadow / base */
+  text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1);
 }
 
-.corner {
-  position: relative;
-  border: none;
-  display: inline-block;
-  padding: 2px 20px;
-}
-.corner::before,
-.corner::after {
-  position: absolute;
-  content: '';
-  width: 20px;
-  height: 15px;
-}
+.frame176 {
+  /* Auto layout */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: 48px;
 
-.corner::before {
-  left: 0;
-  top: 0;
-  border-left: 1px solid #dddddd;
-  border-top: 1px solid #dddddd;
-}
-.corner::after {
-  right: 0;
-  bottom: 0;
-  border-right: 1px solid #dddddd;
-  border-bottom: 1px solid #dddddd;
+  width: 960.83px;
+  height: 1878.78px;
+
+  /* Inside auto layout */
+  flex: none;
+  order: 1;
+  flex-grow: 0;
 }
 </style>
