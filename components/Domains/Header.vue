67916c794855e04a296581537bac95629d50f4aa
Hiroya Akita <akky.develop@gmail.com>
2022-07-31 16:54:59 +0900

    PC,SPのヘッダーを修正

--- components/Domains/Header.vue
+++ components/Domains/Header.vue
@@ -13,6 +13,9 @@
       <div
         class="flex flex-row items-center justify-around w-full mx-10 text-xl font-bold font-fira whitespace-nowrap menu"
       >
+        <!--###############-->
+        <!-- 概要セクション -->
+        <!--###############-->
         <div class="relative flex flex-row group">
           <p class="relative">
             {{ $t('common.menu.about.about') }}
@@ -21,73 +24,158 @@
           <div
             class="invisible rounded-md shadow-md child-menu top-7 group-hover:visible"
           >
-            <div class="child-menu-record">
+            <a
+              class="child-menu-record"
+              href="https://drive.google.com/file/d/1iNF7VvDCd_gWDsSn2i5U8FB1IQWkWOM9/view"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div class="flex w-full pt-2 pb-2 ml-6">
                 <div class="arrow-right"></div>
-                <p class="ml-6">COC(行動規範)</p>
+                <p class="ml-6">{{ $t('common.menu.about.coc') }}</p>
+              </div>
+            </a>
+            <!-- <div class="child-menu-record">
+              <div class="flex w-full pt-2 pb-2 ml-6">
+                <div class="arrow-right"></div>
+                <p class="ml-6">{{ $t('common.menu.about.venue') }}</p>
               </div>
             </div>
             <div class="child-menu-record">
               <div class="flex w-full pt-2 pb-2 ml-6">
                 <div class="arrow-right"></div>
-                <p class="ml-6">感染対策ガイドライン</p>
+                <p class="ml-6">{{ $t('common.menu.about.support') }}</p>
               </div>
-            </div>
-            <div class="child-menu-record">
+            </div> -->
+            <!-- TODO: リンクは仮置きのため、修正すること
+            <a
+              class="child-menu-record"
+              href="https://pyconjp.blogspot.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div class="flex w-full pt-2 pb-2 ml-6">
                 <div class="arrow-right"></div>
-                <p class="ml-6">サポート</p>
+                <p class="ml-6">
+                  {{ $t('common.menu.about.COVID-19Guidelines') }}
+                </p>
               </div>
-            </div>
-            <div class="child-menu-record">
+            </a> -->
+            <!-- TODO: リンクは仮置きのため、修正すること
+            <a
+              class="child-menu-record"
+              href="https://pyconjp.blogspot.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div class="flex w-full pt-2 pb-2 ml-6">
                 <div class="arrow-right"></div>
-                <p class="ml-6">会場案内</p>
+                <p class="ml-6">
+                  {{
+                    $t(
+                      'common.menu.about.infectionExpansionPreventionCheckSheet'
+                    )
+                  }}
+                </p>
               </div>
-            </div>
+            </a> -->
           </div>
         </div>
-        <div class="relative flex flex-row group">
+        <!--###############-->
+        <!-- イベントセクション -->
+        <!--###############-->
+        <div class="relative flex flex-row group disable-color">
           <p>{{ $t('common.menu.event.event') }}</p>
           <div class="arrow-bottom"></div>
           <div
             class="invisible rounded-md shadow-md child-menu top-7 group-hover:visible"
           >
+            <!-- <div class="child-menu-record">
+              <div class="flex w-full pt-2 pb-2 ml-6">
+                <div class="arrow-right"></div>
+                <p class="ml-6">{{ $t('common.menu.event.timetable') }}</p>
+              </div>
+            </div>
             <div class="child-menu-record">
               <div class="flex w-full pt-2 pb-2 ml-6">
                 <div class="arrow-right"></div>
-                <p class="ml-6">タイムテーブル</p>
+                <p class="ml-6">{{ $t('common.menu.event.tutorial') }}</p>
               </div>
             </div>
+            <div class="child-menu-record">
+              <div class="flex w-full pt-2 pb-2 ml-6">
+                <div class="arrow-right"></div>
+                <p class="ml-6">{{ $t('common.menu.event.sprint') }}</p>
+              </div>
+            </div> -->
           </div>
         </div>
+        <!--###############-->
+        <!-- スポンサーセクション -->
+        <!--###############-->
         <div class="relative flex flex-row group">
           <p>{{ $t('common.menu.sponsor.sponsor') }}</p>
           <div class="arrow-bottom"></div>
           <div
             class="invisible rounded-md shadow-md child-menu top-7 group-hover:visible"
           >
-            <div class="child-menu-record">
+            <!-- <div class="child-menu-record">
               <div class="flex w-full pt-2 pb-2 ml-6">
                 <div class="arrow-right"></div>
-                <a class="ml-6">スポンサー募集要項</a>
+                <a class="ml-6">{{ $t('common.menu.sponsor.sponsorList') }}</a>
               </div>
-            </div>
+            </div> -->
+            <a
+              class="child-menu-record"
+              href="https://pyconjp.blogspot.com/2022/05/pyconjp2022-sponsorship.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div class="flex w-full pt-2 pb-2 ml-6">
+                <div class="arrow-right"></div>
+                <a class="ml-6">{{
+                  $t('common.menu.sponsor.sponsorApplicationInformation')
+                }}</a>
+              </div>
+            </a>
+            <a
+              class="child-menu-record"
+              href="https://drive.google.com/file/d/1EANBgiaURLUOuZ8HpWtXN8bgvUwMl-Wl/view"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div class="flex w-full pt-2 pb-2 ml-6">
+                <div class="arrow-right"></div>
+                <a class="ml-6">{{
+                  $t('common.menu.sponsor.sponsorApplicationRequirements')
+                }}</a>
+              </div>
+            </a>
           </div>
         </div>
-        <div class="flex flex-row">
+        <!--###############-->
+        <!-- コンテンツセクション -->
+        <!--###############-->
+        <div class="flex flex-row disable-color">
           <p>{{ $t('common.menu.contents.contents') }}</p>
-          <img
+          <!-- 外部へリンクする場合は以下の画像を有効化する -->
+          <!-- <img
             class="inline h-4 mt-1 ml-2"
             src="@/assets/images/link-icon.svg"
             alt="content"
-          />
+          /> -->
         </div>
-        <div class="">
+        <!--###############-->
+        <!-- スタッフセクション -->
+        <!--###############-->
+        <div class="disable-color">
           <p>{{ $t('common.menu.volunteer.volunteer') }}</p>
         </div>
       </div>
 
+      <!--###############-->
+      <!-- 言語切り替えエリア -->
+      <!--###############-->
       <div
         class="flex flex-row pr-6 my-3 font-bold leading-10 bg-secondary-400 whitespace-nowrap font-noto"
       >
@@ -180,14 +268,22 @@ export default {
   background: #1a1623;
   position: absolute;
   border-radius: 2px;
-  width: 260px;
+  width: 270px;
   color: white;
   font-size: 15px;
   font-family: 'Noto Sans JP';
+  white-space: normal;
 }
 
+.child-menu-record {
+  display: block;
+}
 .child-menu-record:hover {
   background: #f3d34f;
   color: black;
+}
+
+.disable-color {
+  opacity: 0.5;
 }
 </style>
