33e4a6e36f3fce65c9823c3025a3a65d2d92d0bb
okusama27 <kaz@iron-hot.com>
2022-08-04 18:41:42 +0900

    Fixed sponsor section

--- components/Domains/TopPage/SponsorSection.vue
+++ components/Domains/TopPage/SponsorSection.vue
@@ -129,8 +129,27 @@
           <h2 class="text-3xl font-bold text-tertiary-900 font-noto">SPECIAL</h2>
           <h4 class="text-lg font-bold text-tertiary-800 font-noto">特別スポンサー</h4>
         </div>
-        <div class="grid grid-cols-4">
-          <!-- 特別スポンサー -->
+        <!-- 特別スポンサー -->
+        <div v-if="sponsors && specials && specials.length > 0">
+          <div class="grid grid-cols-4">
+            <div v-for="special in specials" :key="special.id" class="m-5">
+              <!--ロゴ-->
+              <div class="card_frame bg-tertiary-50 gold_card" >
+                <outer-link :to="special[$i18n.locale].url">
+                  <img 
+                  :src="require(`@/assets/images/sponsors/${special[$i18n.locale].logo_file}`)"
+                  class="object-none object-center w-full h-full" />
+                </outer-link>
+              </div>
+              <!--会社名-->
+              <div class="mt-2 underline">
+                <outer-link :to="special[$i18n.locale].url" class="text-2xl font-bold font-noto text-primary-700">
+                    {{ special[$i18n.locale].name }}
+                    <ExternalLinkIcon class="inline w-4 h-4 mb-2" />
+                </outer-link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       
@@ -142,9 +161,22 @@
           <h2 class="text-3xl font-bold text-tertiary-900 font-noto">PATRON</h2>
           <h4 class="text-lg font-bold text-tertiary-800 font-noto">パトロン</h4>
         </div>
-        <div class="grid grid-cols-4">
-          <!-- パトロン -->
-        </div>
+        <!-- パトロン -->
+        <div v-if="sponsors && patrons && patrons.length > 0">
+            <div class="grid grid-cols-3">
+              <div v-for="patron in patrons" :key="patron.id" class="m-5">
+                <div class="mt-2 underline">
+                  <img 
+                    :src="require(`@/assets/images/sponsors/${patron[$i18n.locale].logo_file}`)"
+                    class="inline object-none object-center mr-2" />
+                  <outer-link :to="patron[$i18n.locale].url" class="inline text-2xl font-bold font-noto text-primary-700">
+                      {{ patron[$i18n.locale].name }}
+                      <ExternalLinkIcon class="inline w-4 h-4 mb-2" />
+                  </outer-link>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
       
       <!--snake face-->
@@ -173,15 +205,15 @@ export default {
       golds: [],
       silvers: [],
       specials: [],
-      // patrons: []
+      patrons: [],
     }
   },
   async fetch() {
     this.sponsors = await this.$content("sponsors")
     .only(['body'])
-    .fetch()
+    .fetch();
     
-    this.filter_sponsors()
+    this.filter_sponsors();
   },
   methods: {
     filter_sponsors() {
@@ -190,41 +222,10 @@ export default {
       let g = 0;
       let sl = 0;
       let sp = 0;
+      let pt = 0;
       for(let i=0;i < this.sponsors.body.length;i++){
-        // 言語別データ作成
-        const hashJa = {};
-        const hashEn = {};
-        const hashi18n = {};
         const sponsorRow = this.sponsors.body[i];
-        console.log(sponsorRow); // eslint-disable-line no-console
-
-        // id,実装,実装可能,セクション,name_ja,name_en,担当,掲載情報確定,ロゴ,introduction_ja,introduction_en,link,other,logo_file,sponsor_type
-        // id
-        hashJa.id = sponsorRow.id;
-        hashEn.id = sponsorRow.id;
-        // name
-        hashJa.name = sponsorRow.name_ja;
-        hashEn.name = sponsorRow.name_en;
-        // introduction
-        hashJa.introduction = sponsorRow.introduction_ja;
-        hashEn.introduction = sponsorRow.introduction_en;
-        // url
-        hashJa.url = sponsorRow.url;
-        hashEn.url = sponsorRow.url;
-        // logo_file
-        if(sponsorRow.logo_file.length > 0){
-          hashJa.logo_file = sponsorRow.logo_file;
-          hashEn.logo_file = sponsorRow.logo_file;
-        }else{
-          hashJa.logo_file = "association-logo.svg";
-          hashEn.logo_file = "association-logo.svg";
-        }
-        // sponsor_type
-        hashJa.sponsor_type = sponsorRow.sponsor_type;
-        hashEn.sponsor_type = sponsorRow.sponsor_type;
-        // create hash
-        hashi18n.ja = hashJa;
-        hashi18n.en = hashEn;
+        const hashi18n = this.split_i18n(sponsorRow);
       
         if(sponsorRow.sponsor_type === "diamond"){
           // ダイアモンドスポンサー
@@ -241,8 +242,47 @@ export default {
         }else if(sponsorRow.sponsor_type === "special"){
           // 特別スポンサー
           this.specials[sp++] = hashi18n;
+        }else if(sponsorRow.sponsor_type === "patron"){
+          // パトロンスポンサー
+          this.patrons[pt++] = hashi18n;
         }
       }
+    },
+    split_i18n(row) {
+      // 言語別データ作成
+      const hashJa = {};
+      const hashEn = {};
+      const hashi18n = {};
+
+      // id
+      hashJa.id = row.id;
+      hashEn.id = row.id;
+      // name
+      hashJa.name = row.name_ja;
+      hashEn.name = row.name_en;
+      // introduction
+      if(row.introduction_ja){
+        hashJa.introduction = row.introduction_ja;
+        hashEn.introduction = row.introduction_en;
+      }
+      // url
+      hashJa.url = row.url;
+      hashEn.url = row.url;
+      // logo_file
+      if(row.logo_file && row.logo_file.length > 0){
+        hashJa.logo_file = row.logo_file;
+        hashEn.logo_file = row.logo_file;
+      }
+      // sponsor_type
+      if(row.sponsor_type){
+        hashJa.sponsor_type = row.sponsor_type;
+        hashEn.sponsor_type = row.sponsor_type;
+      }
+      // create hash
+      hashi18n.ja = hashJa;
+      hashi18n.en = hashEn;
+
+      return hashi18n;
     },
   },
 }
