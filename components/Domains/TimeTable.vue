c8258e780de5cc30971cfa453a61214382bcbd7a
Akira Inamori <akira.inamori@tf-ebina.com>
2022-08-06 20:16:25 +0900

    TimeTableのガワだけ作成

---
+++ components/Domains/TimeTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <subpage-hero-section
+      :title="['Time', 'Table']"
+      subtitle="タイムテーブル"
+    />
+    <div class="component-border-top bg-tertiary-100">
+      <div class="flex flex-col items-center">
+        <div class="mt-10 snake-face-base">
+          <!--snake face-->
+          <div class="hidden snake-face lg:block" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SubpageHeroSection from '../Elements/SubpageHeroSection'
+
+export default {
+  name: 'TimeTable',
+  components: {
+    SubpageHeroSection,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+.snake-face-base {
+  width: 100%;
+  height: 90px;
+}
+
+.snake-face {
+  position: absolute;
+  width: 150px;
+  height: 90px;
+  right: 85px;
+
+  background-image: url('@/assets/images/section_bg_img_left_black.svg');
+}
+</style>
+
+<!-- ↓JSONフォーマットです -->
+<i18n>
+  {  }
+</i18n>
---
+++ pages/timetable.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <time-table />
+  </div>
+</template>
+
+<script>
+import TimeTable from '@/components/Domains/TimeTable'
+
+export default {
+  name: 'TimeTablePage',
+  components: {
+    TimeTable,
+  },
+}
+</script>
