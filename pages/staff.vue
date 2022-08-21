fa996d9c9f7d328ef49eaa7a7d31ede28fc9ad90
Akira Inamori <akira.inamori@tf-ebina.com>
2022-08-21 16:53:15 +0900

    StaffPageのワイヤフレームとして作成

---
+++ pages/staff.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <chair-person-section />
+    <staff-section />
+    <day-staff-section />
+    <contact-section />
+  </div>
+</template>
+
+<script>
+import ChairPersonSection from '@/components/Domains/StaffPage/ChairPersonSection'
+import StaffSection from '@/components/Domains/StaffPage/StaffSection'
+import DayStaffSection from '@/components/Domains/StaffPage/DayStaffSection'
+import ContactSection from '@/components/Domains/StaffPage/ContactSection'
+
+export default {
+  name: 'StaffPage',
+  components: {
+    ChairPersonSection,
+    StaffSection,
+    DayStaffSection,
+    ContactSection,
+  },
+}
+</script>
