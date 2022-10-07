f0a88b7eaec322b80a7338a398fdb6c45c954daa
Akira Inamori <ainamori@iij.ad.jp>
2022-10-07 18:09:21 +0900

    Add Chair Message.

--- components/Domains/StaffPage/ChairSection.vue
+++ components/Domains/StaffPage/ChairSection.vue
@@ -24,6 +24,26 @@
                 class="mt-5 font-bold chair-description"
                 v-html="$t('chair.description')"
               ></p>
+              <outer-link
+                :to="$t('chair.twitter')"
+                class="text-2xl font-bold font-noto text-primary-700"
+              >
+                <img
+                  class="inline w-6 h-6"
+                  data-v-bf323f94=""
+                  :src="require('@/assets/images/sns/twitter.svg')"
+                />
+              </outer-link>
+              <outer-link
+                :to="$t('chair.facebook')"
+                class="text-2xl font-bold font-noto text-primary-700"
+              >
+                <img
+                  class="inline w-6 h-6"
+                  data-v-bf323f94=""
+                  :src="require('@/assets/images/sns/facebook.svg')"
+                />
+              </outer-link>
             </div>
           </div>
         </div>
@@ -33,9 +53,13 @@
 </template>
 
 <script>
+import OuterLink from '@/components/Elements/OuterLink'
+
 export default {
   name: 'ChierPersonSection',
-  components: {},
+  components: {
+    OuterLink,
+  },
   data: () => ({}),
 }
 </script>
@@ -65,7 +89,7 @@ export default {
   "ja": {
     "chair": {
       "name": "片寄 里菜",
-      "description": "うさぎ大好き。PyConJP2022座長。車やDrone, Raspberry Piなどガジェットが好きです。ピアノ,バイオリン,ドラムを特訓中。プログラマー、エンジニアではなく科学者(Scientist)を目指す。Pyladies Tokyo Staff, 株式会社moegi代表、航空宇宙学修了。三アマ取得",
+      "description": "PyCon JP 2016に初参加。その後、2017,2018にスピーカー参加し、2020年には招待講演で登壇。海外では、PyCon US、PyCon Taiwan、PyCon Thailandの参加経験あり。うさぎ好き。車やDrone, Raspberry Piなどガジェット好き。ピアノ,バイオリンを特訓中。プログラマー、エンジニアではなく科学者(Scientist)を目指す。Pyladies Tokyo Staff, (株)moegi代表。東海大学 航空宇宙学修了。慶應義塾大学SFC研究所 上席所員",
       "sns": {
         "twitter": "https://twitter.com/selina787b",
         "github": null,
