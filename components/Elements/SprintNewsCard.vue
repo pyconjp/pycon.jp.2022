5a7452fe5e53b783d3c3d849fef44055b963c66c
k-nakamura <koya.nakamura@gmail.com>
2022-10-15 11:00:58 +0900

    スプリントの告知追加

---
+++ components/Elements/SprintNewsCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <news-card-base
+    :primary="primary"
+    :title="$t('title').toString()"
+    :description="$t('description').toString()"
+    :url="$t('url').toString()"
+  >
+    <template #icon>
+      <user-group-icon
+        :class="
+          'h-9 w-9 mr-4 ' +
+          (primary ? 'text-primary-700' : ' text-secondary-400')
+        "
+      />
+    </template>
+  </news-card-base>
+</template>
+
+<script>
+import { UserGroupIcon } from '@vue-hero-icons/solid'
+import NewsCardBase from '@/components/Elements/NewsCardBase'
+
+export default {
+  name: 'SprintNewsCard',
+  components: { NewsCardBase, UserGroupIcon },
+  props: {
+    primary: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
+
+<i18n>
+{
+  "ja": {
+    "title": "スプリントリーダー/参加者募集中",
+    "description": "10/16(日)にスプリントを開催します。スプリント参加にあたってPyCon JP 2022のチケットは必要ありません。",
+    "url": "https://pyconjp.blogspot.com/2022/09/pyconjp2022-sprint-ja.html"
+  },
+  "en": {
+    "title": "Volunteers wanted!",
+    "description": "A sprint will be held on Sunday, October 16. You do not need a ticket for PyCon JP 2022 to participate in the sprint.",
+    "url": "https://pyconjp.blogspot.com/2022/09/pyconjp2022-sprint-en.html"
+  }
+}
+</i18n>
